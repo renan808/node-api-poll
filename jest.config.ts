9091e664c9a3a2ee1d8280619cacdb7ec3947601
exercicios <cookiezdsz@gmail.com>
2023-11-03 14:57:47 -0300

    config for lintstaged and jest added

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from 'jest'
+
+const config: Config = {
+  roots: ['<rootDir/src>'],
+
+  collectCoverage: true,
+
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+
+  coverageDirectory: 'coverage',
+
+  coverageProvider: 'v8',
+
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
+
+export default config
