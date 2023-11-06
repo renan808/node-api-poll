a3fc019aee4e51f5d951b7302b58e340b1e2c832
exercicios <cookiezdsz@gmail.com>
2023-11-06 17:23:14 -0300

    feat: sign up controller return erro in body

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,8 @@
 export class SignUpController {
     handle (httpRequest: any): any {
         return {
-            statuscode: 400
+            statuscode: 400,
+            body: new Error('Missing param: name')
         }
     }
 }
