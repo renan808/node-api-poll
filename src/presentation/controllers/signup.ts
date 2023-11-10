9b60aa141e8e136c6cd53eef222af6fc1c1481ce
exercicios <cookiezdsz@gmail.com>
2023-11-10 16:01:33 -0300

    return 500 if emailvalidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,6 +4,7 @@ import { badRequest } from '../helpers/http-helper'
 import type { Controller } from '../protocols/controller'
 import type { EmailValidator } from '../protocols/EmailValidator'
 import { InvalidParamError } from '../errors/invalid-param-error'
+import { ServerError } from '../errors/Server-error'
 
 export class SignUpController implements Controller {
     private readonly emailValidator: EmailValidator
@@ -12,18 +13,25 @@ export class SignUpController implements Controller {
     }
 
     handle (httpRequest: httpRequest): httpResponse {
-        const requiredFields = ['name', 'password', 'email', 'password_confirm']
-        let missingfield
-        let invalidParam
-        for (const field of requiredFields) {
-            if (!httpRequest.body[field]) {
-                missingfield = field
-                return badRequest(new MissingParamError(missingfield))
+        try {
+            const requiredFields = ['name', 'password', 'email', 'password_confirm']
+            let missingfield
+            let invalidParam
+            for (const field of requiredFields) {
+                if (!httpRequest.body[field]) {
+                    missingfield = field
+                    return badRequest(new MissingParamError(missingfield))
+                }
+            }
+            if (!this.emailValidator.isValid(httpRequest.body.email)) {
+                invalidParam = 'email'
+            }
+            return badRequest(new InvalidParamError(invalidParam))
+        } catch {
+            return {
+                statuscode: 500,
+                body: new ServerError()
             }
         }
-        if (!this.emailValidator.isValid(httpRequest.body.email)) {
-            invalidParam = 'email'
-        }
-        return badRequest(new InvalidParamError(invalidParam))
     }
 }
