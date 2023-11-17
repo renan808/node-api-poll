9962ef637223ab021c21a4197663da9de5dba893
exercicios <cookiezdsz@gmail.com>
2023-11-17 17:11:13 -0300

    test: signup controller calls Addaccount with correct values

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,28 +1,47 @@
 import { SignUpController } from './signup'
 import type { EmailValidator } from '../protocols'
 import { ServerError, InvalidParamError, MissingParamError } from '../errors/'
-
-interface SutTypes {
-  sut: SignUpController
-  emailValidatorStub: EmailValidator
-}
+import type { AddAccountModel, AddAccount } from '../../domain/use-cases/add-account'
+import type { AccountModel } from '../../domain/models/account'
 
 const makeEmailvalidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
     isValid (email: string): boolean {
-      return false
+      return true
     }
   }
   return new EmailValidatorStub()
 }
 
+const makeAddAccount = (): AddAccount => {
+  class AddAccountStub implements AddAccount {
+    add (account: AddAccountModel): AccountModel {
+      const fakeAccount: AccountModel = {
+        id: 'valid_id',
+        name: 'valid_name',
+        email: 'valid_email@email.com',
+        password: 'valid_password'
+      }
+      return fakeAccount
+    }
+  }
+  return new AddAccountStub()
+}
+
+interface SutTypes {
+  sut: SignUpController
+  emailValidatorStub: EmailValidator
+  AddAccountStub: AddAccount
+}
+
 const makeSut = (): SutTypes => {
-  // classe mockada
+  const AddAccountStub = makeAddAccount()
   const emailValidatorStub = makeEmailvalidator()
-  const sut = new SignUpController(emailValidatorStub)
+  const sut = new SignUpController(emailValidatorStub, AddAccountStub)
   return {
     sut,
-    emailValidatorStub
+    emailValidatorStub,
+    AddAccountStub
   }
 }
 
@@ -90,7 +109,7 @@ describe('SignUp Controller', () => {
         email: 'any_email',
         name: 'any_name',
         password: 'any_password',
-        password_confirm: 'invalid_password'
+        password_confirm: 'awdawdawasxafdrsgdtrhg'
       }
     }
     const httpResponse = sut.handle(httpRequest)
@@ -145,5 +164,23 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statuscode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('Should call AddAccount with correct values', () => {
+    const { sut, AddAccountStub } = makeSut()
+    const addSpy = jest.spyOn(AddAccountStub, 'add')
+    const httpRequest = {
+      body: {
+        email: 'valid_email@email.com',
+        name: 'valid_name',
+        password: 'valid_password'
+      }
+    }
+    sut.handle(httpRequest)
+    expect(addSpy).toHaveBeenCalledWith({
+      name: 'valid_name',
+      email: 'valid_email@email.com',
+      password: 'valid_password'
+    })
   })
 })
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,33 +1,44 @@
 import type { httpResponse, httpRequest, Controller, EmailValidator } from '../protocols'
 import { MissingParamError, InvalidParamError } from '../errors/'
 import { badRequest, serverError } from '../helpers/http-helper'
+import type { AddAccount } from '../../domain/use-cases/add-account'
 
 export class SignUpController implements Controller {
     private readonly emailValidator: EmailValidator
-    constructor (emailValidator: EmailValidator) {
+    private readonly AddAccount: AddAccount
+    constructor (emailValidator: EmailValidator, AddAccountx: AddAccount) {
         this.emailValidator = emailValidator
+        this.AddAccount = AddAccountx
     }
 
     handle (httpRequest: httpRequest): httpResponse {
         try {
             const requiredFields = ['name', 'password', 'email', 'password_confirm']
-            let missingfield
-            let invalidParam
-            const { email, password, password_confirm } = httpRequest.body
+            const password = httpRequest.body.password
+            const email = httpRequest.body.email
+            const password_confirm = httpRequest.body.password_confirm
+            const name = httpRequest.body.name
+            this.AddAccount.add({
+                name,
+                email,
+                password
+            })
             for (const field of requiredFields) {
                 if (!httpRequest.body[field]) {
-                    missingfield = field
-                    return badRequest(new MissingParamError(missingfield))
+                    return badRequest(new MissingParamError(field))
                 }
-            }
-            if (!this.emailValidator.isValid(email)) {
-                invalidParam = 'email'
             }
             if (password !== password_confirm) {
                 return badRequest(new InvalidParamError('password_confirm'))
             }
-
-            return badRequest(new InvalidParamError(invalidParam))
+            const isValid = this.emailValidator.isValid(email)
+            if (!isValid) {
+                return badRequest(new InvalidParamError('email'))
+            }
+            return {
+            body: httpRequest,
+            statuscode: 200
+            }
         } catch {
             return serverError()
         }
