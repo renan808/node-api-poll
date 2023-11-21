8832685578a341dbf5cbcc9e2b347ca4839513be
exercicios <cookiezdsz@gmail.com>
2023-11-21 17:32:44 -0300

    test: ensure email validator return true and false

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,7 +1,16 @@
 import { EmailValidatorAdapter } from './email-validator-adapter'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+    isEmail (): boolean {
+        return true
+    }
+}))
+
 describe('EmailValidator Adapter,', () => {
     test('Should return false if validator return false', () => {
         const sut = new EmailValidatorAdapter()
+        jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
         const isValid = sut.isValid('invalid_email@mail.com')
         expect(isValid).toBe(false)
     })
