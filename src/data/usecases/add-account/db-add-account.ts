5e18a272f3da21e54266bca46bc4625d7abedfcb
exercicios <cookiezdsz@gmail.com>
2023-11-22 16:52:40 -0300

    feature and test: db-add-account and ensure encrypt method is passed with correct values

---
+++ src/data/usecases/add-account/db-add-account.ts
@@ -0,0 +1,21 @@
+import type { AccountModel } from '../../../domain/models/account'
+import type { AddAccount, AddAccountModel } from '../../../domain/use-cases/add-account'
+import type { Encrypter } from '../../protocols/encrypter'
+export class DbAddAccount implements AddAccount {
+    private readonly encrypter: Encrypter
+    constructor (encrypter: Encrypter) {
+        this.encrypter = encrypter
+    }
+
+    async add (account: AddAccountModel): Promise<AccountModel> {
+        await this.encrypter.encrypt(account.password)
+        return await new Promise(resolve => {
+            resolve({
+                id: 'valid_id',
+                name: 'valid_name',
+                email: 'valid_email',
+                password: 'valid_password'
+            })
+        })
+    }
+}
