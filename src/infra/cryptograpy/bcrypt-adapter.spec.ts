8111f353b2f3e88f8570675e8f95a322d762facc
exercicios <cookiezdsz@gmail.com>
2023-11-27 15:25:46 -0300

    test: ensure bcryptAdapter throws if bcrypt throws

--- src/infra/cryptograpy/bcrypt-adapter.spec.ts
+++ src/infra/cryptograpy/bcrypt-adapter.spec.ts
@@ -27,4 +27,13 @@ describe('Bcrypt Adapter', () => {
         const hash = await sut.encrypt('any_value')
         expect(hash).toBe('hash')
     })
+
+    test('Should throw if bcrypt throws', async () => {
+        jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+            throw new Error()
+        })
+        const sut = makeSut()
+        const promise = sut.encrypt('any_value')
+        await expect(promise).rejects.toThrow()
+    })
 })
