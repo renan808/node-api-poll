4c8eab6df1818bb4586a00049b694d8a70bf5156
exercicios <cookiezdsz@gmail.com>
2023-12-01 15:11:38 -0300

    move sut accontRepository creation to a factory mathod

--- src/infra/db/mongo-db/account-repository/account.spec.ts
+++ src/infra/db/mongo-db/account-repository/account.spec.ts
@@ -7,9 +7,11 @@ describe('Account Mongo Repository', () => {
     afterAll(async () => {
         await Mongohelper.disconnect()
     })
-
+    const makeSut = (): AccountMongoRepository => {
+        return new AccountMongoRepository()
+    }
     test('Should return an account on success', async () => {
-        const sut = new AccountMongoRepository()
+        const sut = makeSut()
         const account = await sut.add({
             name: 'any_name',
             email: 'any_email@email.com',
