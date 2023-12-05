242f9ea796dd19ecd7f458fa48b4bebed262374a
exercicios <cookiezdsz@gmail.com>
2023-12-05 16:51:55 -0300

    connect to mongodb before all tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,20 @@
 import request from 'supertest'
 import app from '../config/app'
+import { Mongohelper } from '../../infra/db/mongo-db/helpers/mongo-helper'
 
 describe('Signup routes', () => {
+    beforeAll(async () => {
+        await Mongohelper.connect(global.__MONGO_URI__)
+    })
+    afterAll(async () => {
+        await Mongohelper.disconnect()
+    })
+
+    beforeEach(async () => {
+        const accountCollection = await Mongohelper.getCollection('accounts')
+        await accountCollection.deleteMany({})
+    })
+
     test('Should return an account on success', async () => {
         await request(app)
         .post('/api/signup')
