9e715751f03718076c908322214e3a5c1198e42f
exercicios <cookiezdsz@gmail.com>
2023-12-07 16:22:44 -0300

    move sut creation to a factory method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,25 +1,41 @@
 import type { Controller, httpRequest, httpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
 
+const makeController = (): Controller => {
+    class ControllerStub implements Controller {
+        async handle (httpRequest: httpRequest): Promise<httpResponse> {
+            const httpResponse: httpResponse = {
+                body: {
+                    name: 'renan',
+                    email: 'renan@renan.com',
+                    password: '123',
+                    password_confirm: '123'
+                },
+                statuscode: 200
+            }
+            return await new Promise(resolve => resolve(httpResponse))
+        }
+    }
+    return new ControllerStub()
+}
+
+interface SutTypes {
+    sut: LogControllerDecorator
+    controllerStub: Controller
+}
+
+const makeSut = (): SutTypes => {
+    const controllerStub = makeController()
+    const sut = new LogControllerDecorator(controllerStub)
+    return {
+        sut,
+        controllerStub
+    }
+}
+
 describe('SignUp Controller', () => {
     test('Should Call controller handle', async () => {
-        class ControllerStub implements Controller {
-            async handle (httpRequest: httpRequest): Promise<httpResponse> {
-                const httpResponse: httpResponse = {
-                    body: {
-                        name: 'renan',
-                        email: 'renan@renan.com',
-                        password: '123',
-                        password_confirm: '123'
-                    },
-                    statuscode: 200
-                }
-                return await new Promise(resolve => resolve(httpResponse))
-            }
-        }
-        const controllerStub = new ControllerStub()
-        const sut = new LogControllerDecorator(controllerStub)
-        const handleSpy = jest.spyOn(controllerStub, 'handle')
+        const { sut, controllerStub } = makeSut()
         const httpRequest: httpRequest = {
             body: {
                 name: 'renan',
@@ -28,6 +44,7 @@ describe('SignUp Controller', () => {
                 password_confirm: '123'
             }
         }
+        const handleSpy = jest.spyOn(controllerStub, 'handle')
         await sut.handle(httpRequest)
         expect(handleSpy).toHaveBeenCalledWith(httpRequest)
     })
