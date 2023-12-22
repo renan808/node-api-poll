18001a8edabad4a27b8c9c00416a795da38e0c3f
exercicios <cookiezdsz@gmail.com>
2023-12-22 17:06:43 -0300

    test: ensure UpdateAcessTokenRepository return throw if update throws

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -155,4 +155,11 @@ describe('DbAuthentication Usecase', () => {
         await sut.auth(makeFakeAuthentication())
         expect(updateSpy).toHaveBeenCalledWith('any_id', 'any_token')
     })
+
+    test('Should throw if UpdateAcessTokenRepository throws', async () => {
+        const { sut, upDateAcessTokenRepositoryStub } = makeSut()
+        jest.spyOn(upDateAcessTokenRepositoryStub, 'update').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+        const promise = sut.auth(makeFakeAuthentication())
+        await expect(promise).rejects.toThrow()
+    })
 })
