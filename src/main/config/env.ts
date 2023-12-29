c406a119f009fdcbfbbb901f634dc89a93a0d215
exercicios <cookiezdsz@gmail.com>
2023-12-29 17:07:03 -0300

    login-factory created, variable salt and jwtSecret are now global

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,6 @@
 export default {
     mongoUrl: global.__MONGO_URI__ || 'mongodb://127.0.0.1:27017',
-    port: process.env.PORT ?? 5050
+    port: process.env.PORT ?? 5050,
+    salt: 12,
+    jwtSecret: process.env.JWT_SECRET ?? 'secret_key'
 }
--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,7 @@
 import type { Router } from 'express'
 import { makeSignUpController } from '../factories/signup/signup-factory'
 import { adaptRoute } from '../adapters/express-route-adapter'
-
+import { makeLoginController } from '../factories/login/login-factory'
 export default (router: Router): void => {
     // eslint-disable-next-line @typescript-eslint/no-misused-promises
     router.post('/signup', adaptRoute(makeSignUpController()))
