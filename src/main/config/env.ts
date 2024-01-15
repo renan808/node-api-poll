2b6a6156bdc62456d7911e90eb144a588510f637
exercicios <cookiezdsz@gmail.com>
2024-01-15 18:19:29 -0300

    mongo path added in env.ts

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-    mongoUrl: global.__MONGO_URI__ || 'mongodb://127.0.0.1:27017',
+    mongoUrl: global.__MONGO_URI__ || 'mongodb+srv://cookiezdsz:G5RJlMd8MKnEZPwu@cluster0.e7xs43a.mongodb.net/?retryWrites=true&w=majority',
     port: process.env.PORT ?? 5050,
     salt: 12,
     jwtSecret: process.env.JWT_SECRET ?? 'secret_key'
