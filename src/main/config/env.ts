89dd399e5936934bc77ce0b1feea0243b15d60bd
exercicios <cookiezdsz@gmail.com>
2024-01-16 13:33:43 -0300

    mongoUrl changed

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-    mongoUrl: global.__MONGO_URI__ || 'mongodb+srv://cookiezdsz:G5RJlMd8MKnEZPwu@cluster0.e7xs43a.mongodb.net/?retryWrites=true&w=majority',
+    mongoUrl: 'mongodb+srv://cookiezdsz:G5RJlMd8MKnEZPwu@cluster0.e7xs43a.mongodb.net/?retryWrites=true&w=majority',
     port: process.env.PORT ?? 5050,
     salt: 12,
     jwtSecret: process.env.JWT_SECRET ?? 'secret_key'
