a032c88eb20b34e5e06713bbea159efe09eba57f
exercicios <cookiezdsz@gmail.com>
2024-01-24 15:05:42 -0300

    .env removed, and dockerfile and docker-compose added and dev script changed

--- dist/main/config/env.js
+++ dist/main/config/env.js
@@ -2,7 +2,7 @@
 var _a, _b;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    mongoUrl: 'mongodb+srv://cookiezdsz:G5RJlMd8MKnEZPwu@cluster0.e7xs43a.mongodb.net/?retryWrites=true&w=majority',
+    mongoUrl: 'mongodb://mongo:27017',
     port: (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 5050,
     salt: 12,
     jwtSecret: (_b = process.env.JWT_SECRET) !== null && _b !== void 0 ? _b : 'secret_key'
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-    mongoUrl: 'mongodb+srv://cookiezdsz:G5RJlMd8MKnEZPwu@cluster0.e7xs43a.mongodb.net/?retryWrites=true&w=majority',
+    mongoUrl: 'mongodb://mongo:27017',
     port: process.env.PORT ?? 5050,
     salt: 12,
     jwtSecret: process.env.JWT_SECRET ?? 'secret_key'
