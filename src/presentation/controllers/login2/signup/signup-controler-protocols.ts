e7ef562b08506436c653e1548d29b65e5c194e3b
renan808 <cookiezdsz@gmail.com>
2024-02-27 14:22:03 -0300

    login and signup controller folder have been moved to the login folder in src/presentation/controllers

---
+++ src/presentation/controllers/login2/signup/signup-controler-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../../protocols'
+export * from '../../../../domain/use-cases/add-account'
+export * from '../../../../domain/models/account'
+export * from '../../../protocols/validation'
--- src/presentation/controllers/login2/signup/signup-controller.spec.ts
+++ src/presentation/controllers/login2/signup/signup-controller.spec.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from './signup-controller'
 import type { AddAccount, AddAccountModel, AccountModel, httpRequest, Validation } from './signup-controler-protocols'
-import { ServerError, MissingParamError } from '../../errors'
-import { serverError, ok, badRequest } from '../../helpers/http/http-helper'
+import { ServerError, MissingParamError } from '../../../errors'
+import { serverError, ok, badRequest } from '../../../helpers/http/http-helper'
 import type { Authentication, AuthenticationModel } from '../login/login-controler-protocols'
 const makeAddAccount = (): AddAccount => {
   class AddAccountStub implements AddAccount {
