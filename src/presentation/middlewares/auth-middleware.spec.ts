4ac452ea1971333587f731242969525f83b0c17c
exercicios <cookiezdsz@gmail.com>
2024-03-07 17:54:34 -0300

    Should call LoadAccountByToken with correct acess token

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,11 +1,34 @@
 import { AcessDeniedError } from '../errors/index'
 import { forbidden } from '../helpers/http/http-helper'
 import { AuthMiddleware } from './auth-middleware'
+import type { LoadAccountByToken } from '../../domain/use-cases/load-account-by-token'
+import type { AccountModel } from '../../domain/models/account'
+
+const makeLoadAccountByTokenStub = (): LoadAccountByToken => {
+    class LoadAccountByTokenStub implements LoadAccountByToken {
+        async load (acessToken: string, role?: string): Promise<AccountModel | null> {
+            return await new Promise(resolve => resolve(null))
+        }
+    }
+    return new LoadAccountByTokenStub()
+}
 
 describe('Auth Middleware', () => {
     test('Should Return 403 if no acess-token exists in header', async () => {
-        const sut = new AuthMiddleware()
+        const sut = new AuthMiddleware(makeLoadAccountByTokenStub())
         const httpResponse = await sut.handle({})
         expect(httpResponse).toEqual(forbidden(new AcessDeniedError()))
+    })
+
+    test('Should call LoadAccountByToken with correct acess token', async () => {
+        const loadAccountByTokenStub = makeLoadAccountByTokenStub()
+        const loadbyTokenSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+        const sut = new AuthMiddleware(loadAccountByTokenStub)
+        await sut.handle({
+            headers: {
+                'x-acess-token': 'any_token'
+            }
+        })
+        expect(loadbyTokenSpy).toHaveBeenCalledWith('any_token')
     })
 })
--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,8 +1,15 @@
 import type { httpRequest, httpResponse, Middleware } from "../protocols"
 import { AcessDeniedError } from '../errors/index'
 import { forbidden } from '../helpers/http/http-helper'
+import type { LoadAccountByToken } from "../../domain/use-cases/load-account-by-token"
 export class AuthMiddleware implements Middleware {
+    constructor (private readonly loadAccountByToken: LoadAccountByToken) {}
+
     async handle (httpRequest: httpRequest): Promise<httpResponse> {
+        const token = httpRequest.headers?.['x-acess-token']
+        if (token) {
+            await this.loadAccountByToken.load(token)
+        }
         return await new Promise(resolve => resolve(forbidden(new AcessDeniedError())))
     }
 }
