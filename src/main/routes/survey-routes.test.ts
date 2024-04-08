5875bfdd5432a5754b358f55c70957fb92f31360
exercicios <cookiezdsz@gmail.com>
2024-04-08 16:31:27 -0300

    Should return 200 if AddSurveys was called with acessToken and other tests

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,9 +2,19 @@ import request from 'supertest'
 import app from '../config/app'
 import { Mongohelper } from '../../infra/db/mongo-db/helpers/mongo-helper'
 import type { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
+
+const makeFakeAccount = (): any => ({
+    name: 'name_test',
+    email: 'emailtest@email.com',
+    password: 'testpassword',
+    role: 'admin'
+})
 
 describe('Survey routes', () => {
     let SurveyCollection: Collection
+    let AccountCollection: Collection
     beforeAll(async () => {
         await Mongohelper.connect(global.__MONGO_URI__)
     })
@@ -15,6 +25,7 @@ describe('Survey routes', () => {
     beforeEach(async () => {
         SurveyCollection = await Mongohelper.getCollection('surveys')
         await SurveyCollection.deleteMany({})
+        AccountCollection = await Mongohelper.getCollection('accounts')
     })
     describe('POST /surveys', () => {
         test('Should return 403 if AddSurveys was called without acessToken', async () => {
@@ -33,9 +44,19 @@ describe('Survey routes', () => {
             .expect(403)
         })
 
-        test('Should return 403 if LoadSurveys was called without acessToken', async () => {
+        test('Should return 200 if AddSurveys was called with acessToken', async () => {
+            const response = await AccountCollection.insertOne(makeFakeAccount())
+            const acessToken = sign({ id: response.insertedId }, env.jwtSecret)
+            await AccountCollection.updateOne({
+                _id: response.insertedId
+            }, {
+                $set: {
+                    acessToken
+                }
+            })
             await request(app)
-            .get('/api/surveys')
+            .post('/api/surveys')
+            .set('x-acess-token', acessToken)
             .send({
                 question: 'some_question',
                 answers: [{
@@ -46,7 +67,34 @@ describe('Survey routes', () => {
                     image: 'image2'
                 }]
             })
+            .expect(200)
+        })
+    describe('GET /surveys', () => {
+        test('Should return 204 if LoadSurveys was called with acessToken', async () => {
+            const response = await AccountCollection.insertOne(makeFakeAccount())
+            const acessToken = sign({ id: response.insertedId }, env.jwtSecret)
+            await AccountCollection.updateOne({
+                _id: response.insertedId
+            }, {
+                $set: {
+                    acessToken
+                }
+            })
+            await request(app)
+            .get('/api/surveys')
+            .set('x-acess-token', acessToken)
+            .send({
+            })
+            .expect(204)
+        })
+
+        test('Should return 403 if LoadSurveys was called without acessToken', async () => {
+            await request(app)
+            .get('/api/surveys')
+            .send({
+            })
             .expect(403)
         })
+    })
     })
 })
